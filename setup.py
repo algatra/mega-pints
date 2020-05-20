b39e8e5177585792f78ef919e7960f97f54baf1f
Megatruh <55190771+algatra@users.noreply.github.com>
2020-05-21 02:15:06 +0700

    adding auto setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('pip install selenium')
+os.system('clear')
+os.system('pip install colorama')
+os.system('clear')
+os.system('pip install requests')
+os.system('clear')
+
+os.system('pip3 install selenium')
+os.system('clear')
+os.system('pip3 install colorama')
+os.system('clear')
+os.system('pip3 install requests')
+os.system('clear')
